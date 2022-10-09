3b9a445ff16bac86161febae48bc3f7daf3f77ec
cabrit0 <cabrit0o.dev@gmail.com>
2022-10-09 17:59:19 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
-  return <div className="App">
-    <Counter/>
-  </div>;
+  return (
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
 }
 
 export default App;
